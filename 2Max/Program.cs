6d169b70c322fbe2c33e2d5886fccb4a1395b45d
katerine964 <54154276+katerine964@users.noreply.github.com>
2022-10-14 14:43:40 +0300

    Add files via upload

---
+++ 2Max/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+int max1=0;
+int max2=0;
+int n=1;
+while (n!=0)
+{
+    Console.WriteLine("Введите число");
+    int m = Convert.ToInt32(Console.ReadLine());
+    n=m;
+    if(n > max1)
+    {
+        max2=max1;
+        max1=n;
+    }
+    if (n<max1 && n>=max2)
+    {
+        max2=n;
+    }
+    if(n==0)
+    {
+    break;
+    } 
+} 
+Console.WriteLine("второй максимум "+max2);
+
