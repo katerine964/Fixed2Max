de6f0fb32ae3c39025170afc979aeb6f3e1ceee9
katerine964 <54154276+katerine964@users.noreply.github.com>
2022-10-14 14:36:32 +0300

    Add files via upload

---
+++ Task36/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+void InputArray(int[] array) 
+{ 
+    for (int i = 0; i < array.Length; i++) 
+    array[i] = new Random().Next(); 
+} 
+int SumN (int[] array)
+{
+    int sum=0;
+    for (int i = 1; i < array.Length; i=i+2) 
+    {
+        sum=sum+array[i];
+    }
+    return sum;
+}    
+Console.Clear(); 
+Console.Write("Введите кол-во элементов массива: "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+int[] array = new int[n]; 
+InputArray(array); 
+Console.WriteLine($" массив: [{string.Join(", ", array)}]"); 
+Console.Write($"Сумма элементов, стоящих на нечетных позициях: {SumN(array)} ");
