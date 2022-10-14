73926500758d49f50416cf9fdc3d01ae20bd52df
katerine964 <54154276+katerine964@users.noreply.github.com>
2022-10-14 14:35:46 +0300

    Add files via upload

---
+++ task34/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+void InputArray(int[] array) 
+{ 
+    for (int i = 0; i < array.Length; i++) 
+    array[i] = new Random().Next(100, 1000); } 
+int Func (int[] array)
+{
+    int m=0;
+    for (int i = 0; i < array.Length; i++) 
+    {
+        if (array[i]%2==0)
+        m++;
+    }
+    return m;
+}
+Console.Clear(); 
+Console.Write("Введите кол-во элементов массива: "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+int[] array = new int[n]; 
+InputArray(array); 
+Console.WriteLine($"массив: [{string.Join(", ", array)}]");
+Console.Write($"количество четных элементов: {Func(array)} ");
